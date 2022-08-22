411b4c34d0efc8da5cd66fe7fd521cf972d0182c
饭太硬 <107107882+fantaiying7@users.noreply.github.com>
2022-08-23 04:50:06 +0800

    Update 聚合短视频.js

--- XYQxb/聚合短视频.js
+++ XYQxb/聚合短视频.js
@@ -4,7 +4,7 @@
     "UserAgent":"",
     "firstpage": "1",
     "class_url": "http://www.sharenice.net/{cateId}?page={catePg}[firstPage=http://www.sharenice.net/{cateId}]",
-    "class_name": "抖音&快手&微视&火山&场库&体育&美拍&秒拍&全民&梨&好兔&美女&正能量&搞笑&社会&游戏&娱乐&旅游&萌系&生活&音乐&美食&明星&动漫&影视&时尚",
+    "class_name": "快手&微视&场库&美拍&秒拍&全民&抖音&火山&体育&梨&好兔&美女&正能量&搞笑&社会&游戏&娱乐&旅游&萌系&生活&音乐&美食&明星&动漫&影视&时尚",
     "class_value": "douyin&kuaishou&weishi&huoshan&changku&tags/t-5L2T6IKy.html&meipai&miaopai&quanmin&lishipin&haotu&tags/t-576O5aWz.html&tags/t-5q2j6IO96YeP.html&tags/t-5pCe56yR.html&tags/t-56S+5Lya.html&tags/t-5ri45oiP.html&tags/t-5aix5LmQ.html&tags/t-5peF5ri4.html&tags/t-6JCM57O7.html&tags/t-55Sf5rS7.html&tags/t-6Z+z5LmQ.html&tags/t-576O6aOf.html&tags/t-5piO5pif.html&tags/t-5Yqo5ryr.html&tags/t-5b2x6KeG.html&tags/t-5pe25bCa.html",
     "filterdata":{},
     "cat_YN_twice": "1",
