64a64f99647b325a59937cec6b2d76093ed5fe7e
饭太硬 <107107882+fantaiying7@users.noreply.github.com>
2022-09-04 02:24:35 +0800

    Add files via upload

---
+++ json/js/script.js
@@ -0,0 +1,7 @@
+var svgContainer = document.getElementById('svgContainer');
+var animItem = bodymovin.loadAnimation({
+  wrapper: svgContainer,
+  animType: 'svg',
+  loop: true,
+  animationData: JSON.parse(animationData)
+});
