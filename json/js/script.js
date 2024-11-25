d41514c2fe8ba63315967cc7c09e48547963dbdd
饭 <stwuya7@gmail.com>
2024-11-25 16:07:31 +0800

    Delete json directory

--- json/js/script.js
+++
@@ -1,7 +0,0 @@
-var svgContainer = document.getElementById('svgContainer');
-var animItem = bodymovin.loadAnimation({
-  wrapper: svgContainer,
-  animType: 'svg',
-  loop: true,
-  animationData: JSON.parse(animationData)
-});
